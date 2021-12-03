d54c8d38538fe59effd42a7c4aa5fd80afa8fa0e
Emil Hultcrantz <emilhul@kth.se>
2021-12-03 12:51:57 +0100

    Update README and clean code

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# emilhul-sorting
+# Sorting Visualizer by Emil Hultcrantz
+
+## How to install
+
+### Option 1
+
+* Clone repo
+* Build using `cargo build --release` from directory
+* Launch emilhul-sorting.exe
+
+### Option 2
+
+* Download emilhul-sorting.exe
+* Launch emilhul-sorting.exe
+
+## How to use
+
+There's supposed to be button which you use to shuffle and sort the array.
+Well there are, I guess, but not drawn out to the user (Yet!). So here are the invisible controls:
+
+On the x axis:
+
+* 1th third of application window shuffles the array
+* 2nd third of application window sorts the array using insertion sort
+* 3rd third of application window sorts the array using pancake sort  
--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,6 @@ const SCREEN_SIZE: (f32, f32) = (
     GRID_SIZE.1 * GRID_CELL_SIZE
 );
 
-pub fn get_color(value: usize, gradient: &mut Gradient) -> EzColor {
-    let position: f64 = value as f64 / 100.0;
-    let color: Color = gradient.at(position);
-    let rgba: (f64, f64, f64, f64) = color.rgba();
-    EzColor::new(rgba.0 as f32, rgba.1 as f32, rgba.2 as f32, rgba.3 as f32)
-}
-
 enum Algorithm {
     InsertionSort,
     PancakeSort
@@ -38,11 +31,10 @@ struct AppState {
     sorting: bool,
     setup: bool,
     outer_step: usize,
-    inner_step: usize,
 }
 
 impl AppState {
-    fn new(ctx: &mut Context) -> GameResult<AppState> {
+    fn new(_ctx: &mut Context) -> GameResult<AppState> {
         Ok(AppState {
             gradient: {
                 CustomGradient::new()
@@ -59,20 +51,38 @@ impl AppState {
             sorting: false,
             setup: false,
             outer_step: 0,
-            inner_step: 0,
         })
     }
 
+    /// Shuffles self.array
     fn shuffle(&mut self) {
         self.array.shuffle(&mut thread_rng());
     }
 
+    /// Gets color for a value 0 <= x <= 100 from self.grardient
+    fn get_color(&mut self, value: usize) -> EzColor {
+        if value <= 100 {
+            let position: f64 = value as f64 / 100.0;
+            let rgba: (f64, f64, f64, f64) = self.gradient.at(position).rgba();
+            EzColor::new(rgba.0 as f32, rgba.1 as f32, rgba.2 as f32, rgba.3 as f32)
+        } else {
+            EzColor::WHITE
+        }
+    }
+
+    // #region Pancake sort
+    /// Set-up for self.pancake_sort()
     fn pancake_sort_setup(&mut self) {
         self.outer_step = self.array.len();
         self.sorting = true;
         self.setup = false;
     }
 
+    /// One step of pancake_sort
+    /// 
+    /// While sorting runs once every frame
+    /// 
+    /// See https://en.wikipedia.org/wiki/Pancake_sorting
     fn pancake_sort(&mut self) {
         if self.outer_step > 1 {
             let mut max_index = 0;
@@ -92,6 +102,9 @@ impl AppState {
         }
     }
 
+    /// Reverses self.array from index 0 to upper_index
+    /// 
+    /// Helper function for self.pancake_sort()
     fn pancake_flipper(&mut self, mut upper_index: usize) {
         let mut lower_index = 0;
         while lower_index < upper_index {
@@ -100,13 +113,26 @@ impl AppState {
             upper_index -= 1;
         }
     }
+    // #endregion
 
+    // #region Insertion sort
+    /// Set-up for self.insertion_sort()
     fn insertion_sort_setup(&mut self) {
         self.outer_step = 1;
         self.sorting = true;
         self.setup = false;
     }
 
+    /// One step of insertion_sort
+    ///  
+    /// While sorting runs once every frame
+    /// 
+    /// To avoid subtraction overflow:
+    /// * j index is equal to outer_step (i), instead of i - 1 
+    /// * stops at j = 1, instead of 0
+    /// * indexes array with j-1, instead of j ( ( j-1 ) + 1 = j )
+    /// 
+    /// See https://en.wikipedia.org/wiki/Insertion_sort
     fn insertion_sort(&mut self) {
         if self.outer_step < self.array.len() {
             let value = self.array[self.outer_step];
@@ -123,11 +149,12 @@ impl AppState {
             self.sorting = false;
         }
     }
+    // #endregion
 }
 
 impl event::EventHandler<GameError> for AppState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
-        while timer::check_update_time(ctx, self.target_fps) {
+        while timer::check_update_time(ctx, self.target_fps) { // Updates at most target_fps frames per second. Can be used to change speed of algorithm
             if self.setup {
                 match self.algorithm {
                     Algorithm::InsertionSort => self.insertion_sort_setup(),
@@ -155,7 +182,7 @@ impl event::EventHandler<GameError> for AppState {
                     40.0 * GRID_CELL_SIZE - self.array[i] as f32 * (30.0 * GRID_CELL_SIZE)/100.0,
                     GRID_CELL_SIZE-5.0,
                     self.array[i] as f32 * (30.0 * GRID_CELL_SIZE)/100.0),
-                get_color(self.array[i], &mut self.gradient)
+                self.get_color(self.array[i])
             )
             .expect("Failed to create bar");
             graphics::draw(ctx, &rectangle, graphics::DrawParam::default())
@@ -173,10 +200,10 @@ impl event::EventHandler<GameError> for AppState {
         _ctx: &mut Context,
         button: event::MouseButton,
         x: f32,
-        y: f32,
+        _y: f32,
     ) {
         if button == event::MouseButton::Left {
-            if !self.sorting {
+            if !self.sorting { // Lock input if sorting
                 if x <= SCREEN_SIZE.0/3.0 {
                     println!{"Shuffle!"};
                     self.shuffle();
@@ -194,7 +221,8 @@ impl event::EventHandler<GameError> for AppState {
     }
 }
 
-pub fn main() {
+/// Sets-up and launches application
+pub fn main() -> GameResult {
     let context_builder = ContextBuilder::new("sorting_visualizer", "Emil Hultcrantz")
         .window_setup(
             conf::WindowSetup::default()
@@ -212,8 +240,112 @@ pub fn main() {
 
 #[cfg(test)]
 mod test {
+    use crate::AppState;
+    use crate::Algorithm;
+    use colorgrad::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2+2,4);
+        assert_eq!(2+2,4)
+    }
+    
+    #[test]
+    fn pancake_sort_test() {
+        print!("Pancake sort: Empty array ... ");
+        let mut test_state = AppState {
+            gradient: {
+                CustomGradient::new()
+                .colors(&[
+                    Color::from_rgba_u8(252, 70, 107, 1*255),
+                    Color::from_rgba_u8(63, 94, 251, 1*255)
+                ])
+                .build()
+                .expect("Failed to build gradient")
+            },
+            target_fps: 60,
+            array: Vec::new(),
+            algorithm: Algorithm::PancakeSort,
+            sorting: false,
+            setup: false,
+            outer_step: 0,
+        };
+
+        test_state.shuffle();
+        test_state.pancake_sort_setup();
+        while test_state.sorting {
+            test_state.pancake_sort();
+        }
+        assert_eq!(test_state.array, []);
+        println!("ok");
+
+        print!("Pancake sort: Repetion of elements ... ");
+        test_state.array = Vec::from([4,2,1,3,5,2,6]);
+        test_state.shuffle();
+        test_state.pancake_sort_setup();
+        while test_state.sorting {
+            test_state.pancake_sort();
+        }
+        assert_eq!(test_state.array, [1,2,2,3,4,5,6]);
+        println!("ok");
+
+        print!("Pancake sort: Reverse input ... ");
+        test_state.array = (1..101).rev().collect();
+        test_state.pancake_sort_setup();
+        while test_state.sorting {
+            test_state.pancake_sort();
+        }
+        let eq: Vec<usize> = (1..101).collect();
+        assert_eq!(test_state.array, eq);
+        println!(" ok");
+    }
+
+    #[test]
+    fn insertion_sort_test() {
+        print!("Insertion sort: Empty array ... ");
+        let mut test_state = AppState {
+            gradient: {
+                CustomGradient::new()
+                .colors(&[
+                    Color::from_rgba_u8(252, 70, 107, 1*255),
+                    Color::from_rgba_u8(63, 94, 251, 1*255)
+                ])
+                .build()
+                .expect("Failed to build gradient")
+            },
+            target_fps: 60,
+            array: Vec::new(),
+            algorithm: Algorithm::InsertionSort,
+            sorting: false,
+            setup: false,
+            outer_step: 0,
+        };
+
+        test_state.shuffle();
+        test_state.pancake_sort_setup();
+        while test_state.sorting {
+            test_state.pancake_sort();
+        }
+        assert_eq!(test_state.array, []);
+        println!("ok");
+
+        print!("Insertion sort: Repetion of elements ... ");
+        test_state.array = Vec::from([4,2,1,3,5,2,6]);
+        test_state.shuffle();
+        test_state.pancake_sort_setup();
+        while test_state.sorting {
+            test_state.pancake_sort();
+        }
+        assert_eq!(test_state.array, [1,2,2,3,4,5,6]);
+        println!("ok");
+
+        print!("Insertion sort: Reverse input ... ");
+        test_state.array = (1..101).rev().collect();
+        test_state.pancake_sort_setup();
+        while test_state.sorting {
+            test_state.pancake_sort();
+        }
+        let eq: Vec<usize> = (1..101).collect();
+        assert_eq!(test_state.array, eq);
+        println!(" ok");
     }
 }
